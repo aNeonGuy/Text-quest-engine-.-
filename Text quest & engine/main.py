0834ab8e1fd788f509105a63fa7311908fc75fd6
Composer <150128043+aNeonGuy@users.noreply.github.com>
2025-11-14 02:18:50 +0300

    updating files

--- Text quest & engine/main.py
+++ Text quest & engine/main.py
@@ -1,24 +1,23 @@
 import json
-cur_id = "0"
-name = "start"
-next = ["1", "2"]
-finish = "0"
-while finish != "1":
-    print("want transition? type number:")
-    for next_id in next:
-        print(f"{next_id}. loc{next_id}")
-    new_cur_id = str(input())
-    with open("data/locations.json") as file:
-        data = json.load(file)
+cur_id = "start"
+game = "0"
+with open("data/locations.json", encoding="utf-8") as file:
+    data = json.load(file)
+while game != "1":
+    name = data[cur_id]["name"]
+    next = data[cur_id]["next"]
+    user_next = data[cur_id]["user_next"]
+    print("Какой хотите совершить переход? Впишите номер локации.")
+    n = 1
+    for next_id in user_next:
+        print(f"{n}. {next_id}")
+        n += 1
+    new_cur_id = next[int(input()) - 1]
     cur_id = new_cur_id
-    name = data[new_cur_id]["name"]
-    next = data[new_cur_id]["next"]
-    if "finish" in data[new_cur_id]:
-        finish = data[new_cur_id]["finish"]
-    if finish == "1":
-        print("want to play again? type 'Yes' if you want, else type something else")
-        if input() == "Yes":
-            cur_id = "0"
-            name = "start"
-            next = ["1", "2"]
-            finish = "0"
+    if "game" in data[new_cur_id]:
+        game = data[new_cur_id]["game"]
+    if game == "1":
+        print("Хотите сыграть снова? Пишите 'да' если хотите.")
+        if input().lower() == "да":
+            cur_id = "start"
+            game = "0"
