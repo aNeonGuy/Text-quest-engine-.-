bd6770a835540b64d703c9142ce3fd3e150ba5d7
Composer <150128043+aNeonGuy@users.noreply.github.com>
2025-12-18 05:57:13 +0300

    Add files via upload

--- Text quest & engine/main.py
+++ Text quest & engine/main.py
@@ -1,21 +1,47 @@
-import json
+import json, random
 cur_id = "start"
 game = "0"
 text = ""
+print("𝕺𝖕𝖊𝖓𝖎𝖓𝖌")
+print("Это введение в сюжет.\nНа дворе 4823, разгар Средневековья. Вы - житель королевства Пельменум, города Сурград.\nВолшебник-недотёпа - это про вас.\nБудучи магом огня, вы не умете кастовать атакующую магию!\nА также вы больны очень тяжёлой болезнью, название ей 'Ротум Вирусум'.\nПричём, так как вам было лень, вы настолько долго не выходили из дома, что если вы сегодня не получите лекарства, вы умрёте.\nУдачи!")
 with open("data/locations.json", encoding="utf-8") as file:
     data = json.load(file)
 while game != "1":
+    k = 1
     name = data[cur_id]["name"]
     next = data[cur_id]["next"]
     user_next = data[cur_id]["user_next"]
-    text = data[cur_id]["text"]
-    for line in text:
+    textstart = data[cur_id]["textstart"]
+    textend = data[cur_id]["textend"]
+    for line in textstart:
         print(line)
-    n = 1
-    for next_id in user_next:
-        print(f"{n}. {next_id}")
-        n += 1
-    new_cur_id = next[int(input()) - 1]
+    if "quiz" in data[cur_id]:
+        print("⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡\n")
+        quiz = data[cur_id]["quiz"]
+        text = data[quiz]["text"]
+        print(text)
+        answ = data[quiz]["answers"]
+        for ans in answ:
+            print(ans)
+        print("(введите выбор целиком)")
+        r = data[quiz]["r"]
+        a = input()
+        if a != r:
+            new_cur_id = data[quiz]["d"]
+            k = 0
+    if k == 1:
+        print("⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡\n")
+        print(textend)
+        n = 1
+        for next_id in user_next:
+            print(f"{n}. {next_id}")
+            n += 1
+        print("(введите число перед ответом без точки)")
+        new_cur_id = next[int(input()) - 1]
+    
+    rand = random.randrange(0, 201)
+    if 73 == rand:
+        new_cur_id = "b_end"
     cur_id = new_cur_id
     if "game" in data[cur_id]:
         game = data[cur_id]["game"]
@@ -27,4 +53,5 @@ while game != "1":
         if input().lower() == "да":
             cur_id = "start"
             game = "0"
+            k = 1
     print("⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡⊡\n")
